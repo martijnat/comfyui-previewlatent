fb6f120df9bc3453ea4baf3b42cd2209a07dfc2d
Martijn Terpstra <bigmartijn@gmail.com>
2023-11-11 15:01:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from .nodes import PreviewLatent
+from .nodes import PreviewLatent,PreviewLatentAdvanced
 
 NODE_CLASS_MAPPINGS = {
     "PreviewLatent": PreviewLatent,
+    "PreviewLatentAdvanced": PreviewLatentAdvanced,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PreviewLatent": "Preview Latent",
+    "PreviewLatentAdvanced": "Preview Latent (Advanced)",
 }
